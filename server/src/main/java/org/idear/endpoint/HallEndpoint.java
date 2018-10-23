c9cd62f265411de8d628bc64336c756395808c96
anyly <idearfly@qq.com>
2018-10-23 21:55:41 +0800

    登录已切换timeline

--- server/src/main/java/org/idear/endpoint/HallEndpoint.java
+++ server/src/main/java/org/idear/endpoint/HallEndpoint.java
@@ -1,13 +1,8 @@
 package org.idear.endpoint;
 
-import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.TypeReference;
-import org.idear.handler.GameCenter;
-import org.idear.timeline.websocket.GameEndpoint;
+import com.idearfly.timeline.websocket.GameEndpoint;
 
-import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
-import java.util.LinkedList;
 
 /**
  * Created by idear on 2018/9/21.
@@ -16,80 +11,15 @@ import java.util.LinkedList;
 @ServerEndpoint("/hall")
 public class HallEndpoint extends GameEndpoint {
 
-    @Override
-    public int onNewGame(JSONObject data) {
-        return super.onNewGame(data);
-
-    }
-
-//    public JSONObject onNewGame1(JSONObject data) {
-//        requireLogin();
-//        LinkedList<String> setting = data.getObject("poker", new TypeReference<LinkedList<String>>(){});
-//        int no = gameCenter.newGame(setting).getNo();
-//        JSONObject jsonObject = new JSONObject();
-//        jsonObject.put("no", no);
-//        return jsonObject;
-//    }
-
-    public JSONObject onFindGame(JSONObject data) {
-        Integer no = data.getInteger("no");
+    public boolean onFindGame(Integer no) {
         if (no == null) {
-            return null;
+            return false;
         }
-        boolean exist = gameCenter.game(no) != null;
-
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("exist", exist);
-        return jsonObject;
+        return gameCenter.game(no) != null;
     }
 
-    @Override
-    public JSONObject onLogin(JSONObject data) {
-        String user = data.getString("user");
-        boolean exist = false;
-        if (user.equals(this.user)) {
-            exist = true;
-        } else {
-            exist = userSession.containsKey(user);
-        }
-
-        JSONObject jsonObject = null;
-//        if (exist) {
-//            jsonObject = new JSONObject();
-//            jsonObject.put("error", "用户名已存在");
-//            return jsonObject;
-//        }
-        return super.onLogin(data);
-    }
-
-    public JSONObject onEditImg(JSONObject data) {
-        requireLogin();
-        this.img = data.getString("img");
-
-        return null;
-    }
-
-    @OnMessage
-    public void onMessage(String message, Session session) {
-        super.onMessage(message);
-    }
-
-
-    @OnOpen
-    public void onOpen(Session session, EndpointConfig config) {
-        super.onOpen(session, config);
-    }
-
-
-    @OnClose
-    public void onClose(Session session, CloseReason closeReason) {
-        super.onClose(closeReason);
-    }
-
-
-    @OnError
-    public void onError(Session session, Throwable error) {
-        super.onError(session, error);
+    public void onEditImg(String img) {
+        this.img = img;
     }
 
 }
