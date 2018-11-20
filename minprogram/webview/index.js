b26e7d87508cb9e2731d4d1089bc8fbbf4fbf226
anyly <idearfly@qq.com>
2018-11-21 00:31:40 +0800

    微信端语音错误 operateRecorder:fail:start record fail

--- minprogram/webview/index.js
+++ minprogram/webview/index.js
@@ -2,61 +2,59 @@ Page({
   onLoad: function (option) {
     var that = this;
     
-    wx.authorize({
-      scope: 'scope.record',
-      success() {
-        // 识别授权
-        console.log("录音授权成功");
-        //第一次成功授权后 状态切换为2
-        var urlparams = '';
-        if (option) {
-          if (option.user) {
-            urlparams += '#login?user=' + option.user + '&img=' + option.img;
-          }
-        }
-        that.setData({
-          'urlparams': urlparams
-        });
-        // 用户已经同意小程序使用录音功能，后续调用 wx.startRecord 接口不会弹窗询问
-        // wx.startRecord();
-        ////使用新版录音接口，可以获取录音文件
-      },
-      fail() {
-        wx.showModal({
-          title: '提示',
-          content: '您未授权录音，功能将无法使用',
-          showCancel: true,
-          confirmText: "授权",
-          confirmColor: "#52a2d8",
-          success: function (res) {
-          },
-          fail: function () {
-            console.log("openfail");
-          }
-        });
+    var urlparams = '';
+    if (option) {
+      if (option.user) {
+        urlparams += '#login?user=' + option.user + '&img=' + option.img;
       }
+    }
+    that.setData({
+      'urlparams': urlparams
     });
+      
     
   },
   startRecord() {
+    var that = this;
     if (this.recordStatus != 'start') {
       this.recorderManager = wx.getRecorderManager();
-      const options = {
+      var options = {
         duration: 6000,
         sampleRate: 44100,
         numberOfChannels: 1,
         encodeBitRate: 220500,
         format: 'wav',
       }
-      this.recorderManager.start(options);
-      this.recordStatus = 'start';
+      wx.authorize({
+        scope: 'scope.record',
+        success() {
+          // 识别授权
+          console.log("录音授权成功");
+          that.recorderManager.start(options);
+          that.recordStatus = 'start';
 
-      this.recorderManager.onError(function(res) {
-        console.log('recorder error', res);
-      });
-      this.recorderManager.onStop(function(res) {
-        console.log('recorder stop', res)
-        this.tempFilePath = res.tempFilePath;
+          that.recorderManager.onError(function (res) {
+            console.log('recorder error', res);
+          });
+          that.recorderManager.onStop(function (res) {
+            console.log('recorder stop', res)
+            that.tempFilePath = res.tempFilePath;
+          });
+        },
+        fail() {
+          wx.showModal({
+            title: '提示',
+            content: '您未授权录音，功能将无法使用',
+            showCancel: true,
+            confirmText: "授权",
+            confirmColor: "#52a2d8",
+            success: function (res) {
+            },
+            fail: function () {
+              console.log("openfail");
+            }
+          });
+        }
       });
     }
   },
